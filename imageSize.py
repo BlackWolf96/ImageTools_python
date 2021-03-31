2c12a2b1f07780f84b91c08908e9c2775e509bfa
BlackWolf96 <oskar.blackwolf@gmail.com>
2021-03-31 23:50:00 +0200

    Add

---
+++ imageSize.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import os
+
+# Image size x, y PX
+imgSize = (2560,1440)
+
+# Get script directory
+directory = os.getcwd()
+
+# Check output dir 
+if(os.path.isdir(os.getcwd()+'/output') == True):
+    # if output dir is exists
+    print('Ten folder juz istnieje')
+else:
+    # create dir
+    os.mkdir('output')
+
+for filename in os.listdir(directory):
+    # If file type is
+    if filename.endswith((".jpg",".jpeg",".png")):
+        # Load image
+        img = Image.open(filename)
+        # Resize img
+        resized = img.resize(imgSize)
+        # Generate file name
+        nameGenerated = 'IMG_'+filename
+        # Save image to output directory
+        resized.save(directory+'/output/'+nameGenerated)
+        continue
+    else:
+        continue
