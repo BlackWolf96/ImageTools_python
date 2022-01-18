1e042ad053a8172f9dfc77dc8c1352968d1484fb
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-01-18 11:42:11 +0100

    Update

--- imageSize.py
+++ imageSize.py
@@ -1,35 +1,34 @@
-from PIL import Image
-import Tkinter
+import tkinter as tk
+from tkinter import filedialog
 
-# Put this script into your photos directory then run it 
 
-# Image size x, y PX
-imgSize = (2560,1440)
 
-# Get script directory
-directory = os.getcwd()
 
-# Check output dir 
-if(os.path.isdir(os.getcwd()+'/output') == True):
-    # if output dir is exists
-    print('Ten folder juz istnieje')
-else:
-    # create dir
-    os.mkdir('output')
 
-for filename in os.listdir(directory):
-    # If file type is
-    if filename.endswith((".jpg",".jpeg",".png")):
-        # Load image
-        img = Image.open(filename)
-        # Resize img
-        resized = img.resize(imgSize)
-        # Generate file name
-        nameGenerated = 'IMG_'+filename
-        # Save image to output directory
-        resized.save(directory+'/output/'+nameGenerated)
-        continue
-    else:
-        continue
 
-    
+class App(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+        self.btn1 = tk.Button(self, width=10, text="Open", command=self.ImageOpen)
+        self.btn1.grid(row=0,column=0, pady=5, padx=5)
+        self.btn2 = tk.Button(self, width=10, text="Edit", command=self.Edit)
+        self.btn2.grid(row=0,column=1, pady=5, padx=5)
+
+
+
+
+    def ImageOpen(self):
+        f_types = [('Jpg Files', '*.jpg')]
+        self.filename = tk.filedialog.askopenfilename(title='open',filetypes=f_types)
+
+    def Edit(self):
+        print('edit')
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('IE')
+    root.geometry("800x600")
+    App(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
