6b4ead58b1a10dd072d55dc3301cb7f42b6a6b41
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-01-18 13:00:21 +0100

    Update

--- imageSize.py
+++ imageSize.py
@@ -1,9 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
-
-
-
-
+from PIL import Image
 
 
 class App(tk.Frame):
@@ -15,20 +12,30 @@ class App(tk.Frame):
         self.btn1.grid(row=0,column=0, pady=5, padx=5)
         self.btn2 = tk.Button(self, width=10, text="Edit", command=self.Edit)
         self.btn2.grid(row=0,column=1, pady=5, padx=5)
-
-
-
+        self.var1 = tk.IntVar()
+        self.check1 = tk.Checkbutton(self, text="Resize", variable=self.var1, onvalue=1, offvalue=0)
+        self.check1.grid(row=0, column=2, pady=5, padx=5)
+        
+        
+        
 
     def ImageOpen(self):
-        f_types = [('Jpg Files', '*.jpg')]
+        f_types = [('Jpg Files', '*.jpg'),('PNG Files','*.png')]
         self.filename = tk.filedialog.askopenfilename(title='open',filetypes=f_types)
 
     def Edit(self):
-        print('edit')
+        print(self.var1.get())
+        if( self.var1.get() == True):
+            re = (800,600)
+            img = Image.open(self.filename)
+            resized = img.resize(re)
+            resized.save('item.png')
+
+            
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title('IE')
+    root.title('Image Editor')
     root.geometry("800x600")
     App(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
