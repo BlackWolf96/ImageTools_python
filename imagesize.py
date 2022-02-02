f6acd91eb7f41af4cdcae743a4d1a671caabb3ad
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-02 02:00:38 +0100

    ADD

--- imagesize.py
+++ imagesize.py
@@ -5,6 +5,11 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
+        self.btn1 = tk.Button(self, text="Convert", command=self.convert)
+        self.btn1.grid(row=0, column=1)
+
+        def convert(self):
+          print('Work')
 
 if __name__ == "__main__":
     root = tk.Tk()
