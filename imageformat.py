c7c7af135da1339cd90994e9016ccd12b1f543c1
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-02 13:31:43 +0100

    update

--- imageformat.py
+++ imageformat.py
@@ -7,11 +7,10 @@ class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.configure(background='white')
         self.grid_columnconfigure(0, weight=2)
         self.grid_rowconfigure(0, weight=2)
         self.grid_columnconfigure(1, weight=2)
-
-
 
         def OpenImage():
           self.imgfile = filedialog.askopenfilename()
@@ -23,21 +22,19 @@ class MainApplication(tk.Frame):
 
         # Items
         #self.check = tk.Checkbutton(self, text="webp", textvariable=self.var)
-        self.label = tk.Label(self, font=(Arial Blod, 11)text="Convert img to webp")
-        self.btn1 = tk.Button(self, text="open", width=10, command=OpenImage)
-        self.btn2 = tk.Button(self, text="Convert", width=10, command=ConvertImage)
+        self.label = tk.Label(self, font=('Arial Bold', 11), fg='black', background='white', text="Convert Image to WEBP")
+        self.btn1 = tk.Button(self, text="Open", fg='white', background='black', width=10, command=OpenImage)
+        self.btn2 = tk.Button(self, text="Convert",fg='white', background='black', width=10, command=ConvertImage)
 
         # Grid
         self.label.grid(row=0, column=0, columnspan=2)
-
-
-
         self.btn1.grid(row=1,column=0,  padx=5, pady=5)
         self.btn2.grid(row=1,column=1, padx=5, pady=5)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title('Image Format')
-    root.geometry("200x150")
+    root.geometry("250x150")
+    root.maxsize(250,150)
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
