3de35cccf78ada004e0db7cf89eff76e1d87f686
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-02 13:18:45 +0100

    update

--- imageformat.py
+++ imageformat.py
@@ -7,6 +7,11 @@ class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.grid_columnconfigure(0, weight=2)
+        self.grid_rowconfigure(0, weight=2)
+        self.grid_columnconfigure(1, weight=2)
+
+
 
         def OpenImage():
           self.imgfile = filedialog.askopenfilename()
@@ -14,25 +19,25 @@ class MainApplication(tk.Frame):
 
         def ConvertImage():
           image = Image.open( self.imgfile ).convert('RGB')
-          name = hashlib.md5( self.imgfile.encode('UTF-8')).hexdigest()
-          if self.var.get():
-            print( ' Works ')
-          image.save( name +'.webp', 'WEBP')
-
-        # Vars
-        self.var = tk.IntVar()
+          image.save( hashlib.md5( self.imgfile.encode('UTF-8')).hexdigest() +'.webp', 'WEBP')
 
         # Items
-        self.check = tk.Checkbutton(self, text="webp", textvariable=self.var)
-        self.btn1 = tk.Button(self, text="open", command=OpenImage)
-        self.btn2 = tk.Button(self, text="Convert", command=ConvertImage)
+        #self.check = tk.Checkbutton(self, text="webp", textvariable=self.var)
+        self.label = tk.Label(self, text="Convert img to webp")
+        self.btn1 = tk.Button(self, text="open", width=10, command=OpenImage)
+        self.btn2 = tk.Button(self, text="Convert", width=10, command=ConvertImage)
 
         # Grid
-        self.btn1.grid(row=0,column=0)
-        self.btn2.grid(row=0,column=1)
+        self.label.grid(row=0, column=0, columnspan=2)
+
+
+
+        self.btn1.grid(row=1,column=0,  padx=5, pady=5)
+        self.btn2.grid(row=1,column=1, padx=5, pady=5)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title('Image Format')
+    root.geometry("200x150")
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
