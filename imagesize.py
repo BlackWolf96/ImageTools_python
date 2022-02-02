88bb74f614ddad7d40306757b9c3dfa371732bae
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-02 02:12:05 +0100

    ADD

--- imagesize.py
+++ imagesize.py
@@ -4,22 +4,29 @@ class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        def OpenImage():
+          print('Elo')
 
-        def convert():
-          print('Work')
+        def ConvertImage():
+          x = self.width.get()
+          y = self.height.get()
+
         
-        self.ent1 = tk.Entry(self)
+        self.width = tk.IntVar()
+        self.height = tk.IntWar()
+        self.ent1 = tk.Entry(self, textvariable=self.width)
         self.ent1.grid(row=0, column=0)
-        self.ent2 = tk.Entry(self)
+        self.ent2 = tk.Entry(self, textvariable=self.height)
         self.ent2.grid(row=0, column=1)
-        self.btn1 = tk.Button(self, text="Convert", command=convert)
+        self.btn1 = tk.Button(self, text="OpenFile", command=OpenImage)
         self.btn1.grid(row=0, column=2)
-        self.btn2 = tk.Button(self, text="Convert", command=convert)
-        self.btn2.grid(row=0, column=2)
+        self.btn2 = tk.Button(self, text="Convert", command=ConvertImage)
+        self.btn2.grid(row=0, column=3)
 
         
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.title('Image Size')
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
